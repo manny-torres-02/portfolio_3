63dd88fac8c5dc8646bb71f3e968d5b5319d56b0
Manuel Torres <manueljosetorres02@gmail.com>
2025-11-09 23:02:31 -0500

    add nodemailer piece

--- server/index.js
+++ server/index.js
@@ -18,6 +18,16 @@ const contactSchema = z.object({
   message: z.string().min(5),
 });
 
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT || 587),
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
   console.log("sserver sent");
